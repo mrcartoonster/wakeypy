5194a48bde74d3db16bb8c17b79bd5ec2f0ca9fd
Evan Baird <mrcartoonster@icloud.com>
2026-09-23 19:37:42 -0600

    adding spinner

--- src/wakeypy/spinning.py
+++ src/wakeypy/spinning.py
@@ -5,8 +5,11 @@ import time
 import tomllib
 from rich.console import Console
 from wakepy import keep
+from pyfiglet import Figlet
 
-console = Console(width=80)
+console = Console()
+
+fig = Figlet(font="slant")
 
 with open(
     "/Users/evanbaird/Projects/Projects/wakeypy/pyproject.toml",
@@ -37,23 +40,18 @@ def rando_dots():
     return "dots" + str(secrets.choice(range(2, 9)))
 
 
-with console.status(status, spinner=rando_dots()):
-    console.print(
-        rf"""
-                       _
-                      | |
-    __        __ __ _ | | __ ___  _   _  _ __    _   _
-    \ \  /\  / // _` || |/ // _ \| | | || '_  \ | | | |
-     \ \/  \/ /| (_| ||   <|  __/| |_| || |_)  || |_| |
-      \__/\__/  \__,_||_|\_\\___| \__, || .___/  \__, |
-      {version}                      __/ || |       __| |
-                                  |___/ |_|      |___/
-    """,
-        style="bold magenta",
-    )
-    console.print(
-        ":white_heavy_check_mark: System will continue running program\n"
-        ":white_heavy_check_mark: Presentation mode is on. Screensaver and Screenlock will be prevented.",
-        style="green",
-    )
-    {do_something()}
+try:
+    with console.status(status, spinner=rando_dots()):
+        console.print(
+            fig.renderText("WakeyPy"),
+            style="bold magenta"
+        )
+        console.print(f"[bold blue]{version}[/bold blue] :desktop_computer:")
+        console.print(
+            ":white_heavy_check_mark: System will continue running program\n"
+            ":white_heavy_check_mark: Presentation mode is on. Screensaver and Screenlock will be prevented.",
+            style="green",
+        )
+        {do_something()}
+except KeyboardInterrupt:
+    console.print("[bold red]Successfully Exited.[/] [blue]Good Bye!:waving_hand_medium-dark_skin_tone:[/]")
